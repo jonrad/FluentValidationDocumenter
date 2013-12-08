54f79130b03486ff3ccf53e5615b98f4e8f3232e
Jon Radchenko <jonrad@gmail.com>
2013-12-08 14:32:21 -0500

    Get name of model from class

---
+++ FluentValidationWikify.Specs/ClassDocumenterSpecs.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Machine.Specifications;
+using Roslyn.Compilers.CSharp;
+
+namespace FluentValidationWikify.Specs
+{
+    [Subject(typeof(ClassDocumenter))]
+    public class ClassDocumenterSpecs
+    {
+        Establish context = () =>
+        {
+            node = SyntaxTree.ParseText("class ModelValidator:AbstractValidator<Model>{}").GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().First();
+
+            documenter = new ClassDocumenter();
+        };
+
+        It should_be_able_to_process =
+            () => documenter.CanProcess(node).ShouldBeTrue();
+
+        It should_return_required =
+            () => documenter.Get(node).ShouldEqual("Model");
+
+        static ClassDocumenter documenter;
+
+        static ClassDeclarationSyntax node;
+    }
+}
---
+++ FluentValidationWikify/ClassDocumenter.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Roslyn.Compilers.CSharp;
+
+namespace FluentValidationWikify
+{
+    public class ClassDocumenter
+    {
+        public bool CanProcess(ClassDeclarationSyntax node)
+        {
+            return true;
+        }
+
+        public string Get(ClassDeclarationSyntax node)
+        {
+            return node
+                .ChildNodes().OfType<BaseListSyntax>().First()
+                .ChildNodes().OfType<GenericNameSyntax>().First()
+                .ChildNodes().OfType<TypeArgumentListSyntax>().First()
+                .ChildNodes().OfType<IdentifierNameSyntax>().First().Identifier.ValueText;
+        }
+    }
+}
