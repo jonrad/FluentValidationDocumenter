01d3e4056294c1f296abc52dcfe3b80d324b0ca4
Jon Radchenko <jon.radchenko@mlp.com>
2013-12-09 19:41:23 -0500

    Refactor

--- FluentValidationWikify/NodeDocumenters/NotNullDocumenter.cs
+++ FluentValidationWikify/NodeDocumenters/NotNullDocumenter.cs
@@ -1,22 +1,10 @@
-﻿using Roslyn.Compilers.CSharp;
-
-namespace FluentValidationWikify.NodeDocumenters
+﻿namespace FluentValidationWikify.NodeDocumenters
 {
-    public class NotNullDocumenter : MemberAccessExpressionDocumenter
+    public class NotNullDocumenter : RequiredDocumenter
     {
-        public override bool IsNewRule
-        {
-            get { return false; }
-        }
-
         public override string MethodName
         {
             get { return "NotNull"; }
-        }
-
-        public override string Get(SyntaxNode node)
-        {
-            return "Required";
         }
     }
 }
---
+++ FluentValidationWikify/NodeDocumenters/RequiredDocumenter.cs
@@ -0,0 +1,17 @@
+﻿using Roslyn.Compilers.CSharp;
+
+namespace FluentValidationWikify.NodeDocumenters
+{
+    public abstract class RequiredDocumenter : MemberAccessExpressionDocumenter
+    {
+        public override bool IsNewRule
+        {
+            get { return false; }
+        }
+
+        public override string Get(SyntaxNode node)
+        {
+            return "Required";
+        }
+    }
+}
