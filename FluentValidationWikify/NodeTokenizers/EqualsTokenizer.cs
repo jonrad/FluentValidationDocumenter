40d802ef88bc570ff10409ccb551eacfdc03d016
Jon Radchenko <jonrad@gmail.com>
2013-12-14 15:05:51 -0500

    Equals implementation

---
+++ FluentValidationWikify/NodeTokenizers/EqualsTokenizer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Roslyn.Compilers.CSharp;
+
+namespace FluentValidationWikify.NodeTokenizers
+{
+    public class EqualsTokenizer : MemberAccessExpressionTokenizer
+    {
+        public override string MethodName
+        {
+            get { return "Equals"; }
+        }
+
+        public override bool IsNewRule
+        {
+            get { return false; }
+        }
+
+        public override Token Get(SyntaxNode node)
+        {
+            var invocation = (InvocationExpressionSyntax)node;
+            var argument = invocation
+                .ChildNodes().OfType<ArgumentListSyntax>().First()
+                .ChildNodes().OfType<ArgumentSyntax>().First()
+                .ChildNodes().First();
+
+            var literal = argument as LiteralExpressionSyntax;
+
+            if (literal != null)
+            {
+                return new Token("equals", literal.Token.Value);
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
